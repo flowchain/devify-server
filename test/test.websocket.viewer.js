b77f076b84d4cf65adc76ba3c8fd11fd1a167623
Jollen Chen <jollen@moko365.com>
2016-03-12 11:20:10 +0800

    Add test.websocket.viewer.js

---
+++ test/test.websocket.viewer.js
@@ -0,0 +1,14 @@
+var WebSocketClient = require('websocket').client;
+
+var client = new WebSocketClient();
+
+client.on('connect', function(connection) {
+    console.log('WebSocket client connected');
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log("Received: '" + message.utf8Data + "'");
+        }
+    });
+});
+
+client.connect('ws://127.0.0.1:8000/object/5550937980d51931b3000009/viewer', '');
