3dfbefdc8795eba4ae5468c90fe63e78d59d5880
Jollen Chen <jollen@moko365.com>
2016-03-18 14:24:47 +0800

    Add a new test case test.coap.send.js

---
+++ test/test.coap.send.js
@@ -0,0 +1,17 @@
+var coap = require('coap');
+
+var sendNumber = function() {
+    // 1 to 100
+    var number = Math.round(Math.random() * 100 + 1);
+    var obj = {temperature: number, temp: number};
+    var data = JSON.stringify(obj);
+
+    console.log('Pushing: ' + data);
+
+    var clientWriable = coap.request('coap://192.168.1.100:8000/object/5550937980d51931b3000009/send');
+    clientWriable.end(new Buffer(data));
+
+    setTimeout(sendNumber, 1000);
+};
+
+sendNumber();
